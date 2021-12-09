9d68dd64176c776568e6eae1ba89155163a609ac
motiph <edwinj.gutierrezm@gmail.com>
2021-12-09 13:23:34 -0800

    rm domain

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(ciapml.westus2.cloudapp.azure.com www.ciapml.westus2.cloudapp.azure.com)
+domains=(ciapml.westus2.cloudapp.azure.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="edwinj.gutierrezm@gmail.com" # Adding a valid address is strongly recommended
